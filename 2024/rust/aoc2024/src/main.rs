dc53422a93e720d16de747fa20321f04b3610d14
Mikhail Tavarez <miktavarez@gmail.com>
2024-12-03 23:15:50 -0600

    day 1 part 2

--- 2024/rust/aoc2024/src/main.rs
+++ 2024/rust/aoc2024/src/main.rs
@@ -1,8 +1,7 @@
 use std::fs::read_to_string;
 
 
-fn main() {
-    // let mut s = String::new();
+fn part_one() {
     let mut a: Vec<i64> = Vec::new();
     let mut b: Vec<i64> = Vec::new();
     for line in read_to_string("data/input/01.txt").unwrap().lines() {
@@ -18,4 +17,25 @@ fn main() {
         result += (b[i] - a[i]).abs();
     }
     println!("{}", result);
+}
+
+fn part_two() {
+    let mut a: Vec<i64> = Vec::new();
+    let mut b: Vec<i64> = Vec::new();
+    for line in read_to_string("data/input/01.txt").unwrap().lines() {
+        let pair: Vec<&str> = line.split("   ").collect();
+        a.push(pair[0].to_string().parse().unwrap());
+        b.push(pair[1].to_string().parse().unwrap());
+    };
+
+    let mut result: i64 = 0;
+    for i in a.iter() {
+        result += i * b.iter().filter(|&x| *x == *i).count() as i64;
+    }
+    println!("{}", result);
+}
+
+fn main() {
+    part_one();
+    part_two();
 }
